d1e9c7090244a398df067f7631685fe4945f51a8
ffgamer1080-pixel <ffgamer1080@gmail.com>
2026-02-09 11:40:16 +0530

    Modify love message and implement floating hearts
    Updated love message and added floating hearts effect.

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function showLove() {
   document.getElementById("msg").innerHTML =
-  "I Love You So Much ❤️💞";
+    "I Love You Forever ❤️🥰";
 }
+
+// floating hearts
+setInterval(() => {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  heart.innerHTML = "💖";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = (3 + Math.random() * 3) + "s";
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}, 400);
